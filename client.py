33053b380634e4b4fef11e2c66c83e995ad868a7
Greg Perrin <gregoryperrin.ucb@gmail.com>
2021-11-02 22:59:03 -0600

    Updates...

--- client.py
+++ client.py
@@ -1,30 +1,36 @@
+# Client
+
 import socket
 import sys
+import json
+
+fd = open('./data.json')
+data = json.load(fd)
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('localhost', 10000)
-print >>sys.stderr, 'connecting to %s port %s' % server_address
+server_address = ('localhost', 8089)
+print('connecting to {} port {}'.format(*server_address))
 sock.connect(server_address)
 
 try:
-    
+
     # Send data
-    message = 'This is the message.  It will be repeated.'
-    print >>sys.stderr, 'sending "%s"' % message
+    message = json.dumps(data).encode("ascii")
+    print('sending {!r}'.format(message))
     sock.sendall(message)
 
     # Look for the response
     amount_received = 0
     amount_expected = len(message)
-    
+
     while amount_received < amount_expected:
         data = sock.recv(16)
         amount_received += len(data)
-        print >>sys.stderr, 'received "%s"' % data
+        print('received {!r}'.format(data))
 
 finally:
-    print >>sys.stderr, 'closing socket'
+    print('closing socket')
     sock.close()
--- server.py
+++ server.py
@@ -1,33 +1,37 @@
+# Echo server
+
 import socket
-import sys
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the port
-server_address = ('localhost', 10000)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
+server_address = ('localhost', 8089)
+print('starting up on {} port {}'.format(*server_address))
 sock.bind(server_address)
 
-# Bind the socket to the port
-server_address = ('localhost', 10000)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
-sock.bind(server_address)
+# Listen for incoming connections
+sock.listen(1)
 
+while True:
+    # Wait for a connection
+    print('waiting for a connection')
+    connection, client_address = sock.accept()
     try:
-        print >>sys.stderr, 'connection from', client_address
+        print('connection from', client_address)
 
         # Receive the data in small chunks and retransmit it
         while True:
             data = connection.recv(16)
-            print >>sys.stderr, 'received "%s"' % data
+            print('received {!r}'.format(data))
             if data:
-                print >>sys.stderr, 'sending data back to the client'
+                print('sending data back to the client')
                 connection.sendall(data)
             else:
-                print >>sys.stderr, 'no more data from', client_address
+                print('no data from', client_address)
                 break
-            
+
     finally:
         # Clean up the connection
         connection.close()
+
